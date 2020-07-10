63ed5647748278562ff0983410577952cde7cc45
niconap <nico.nap@outlook.com>
2020-07-10 13:32:27 +0200

    Made the calculator completely functional and fixed some display-bugs.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 
+/*
+    Made by Nico Nap
+    GitHub: https://github.com/niconap
+*/
+
 let input = "";
 let inputArray = [];
+let answer = "";
+let updated = false;
 
 /* Functions for doing the basic math operations */
 
@@ -20,18 +27,46 @@ function divide (a, b) {
     return a / b;
 }
 
-function operate(operator, a, b) {
-    if (operator == "add") {
-        return add(a, b);
+function operate(array) {
+    if (array != [] || array.length != 1) {
+        let number1 = parseFloat(array[0]);
+        let number2 = parseFloat(array[2]);
+        let operator = array[1];
+        if (operator == "add") {
+            array[0] = add(number1, number2).toString();
+            array.splice(1, 2);
+        }
+        if (operator == "subtract") {
+            array[0] = subtract(number1, number2).toString();
+            array.splice(1, 2);
+        }
+        if (operator == "multiply") {
+            array[0] = multiply(number1, number2).toString();
+            array.splice(1, 2);
+        }
+        if (operator == "divide") {
+            array[0] = divide(number1, number2).toString();
+            array.splice(1, 2);
+        }
+        if (array.length != 1) {
+            operate(array);
+        }
     }
-    if (operator == "subtract") {
-        return subtract(a, b);
-    }
-    if (operator == "multiply") {
-        return multiply(a, b);
-    }
-    if (operator == "divide") {
-        return divide(a, b);
+    return array[0];
+}
+
+function updateDisplay(answer) {
+    document.getElementById("value").value = answer;
+    updated = true;
+}
+
+function checkIfUpdated() {
+    if (updated == true) {
+        input = "";
+        inputArray = [];
+        answer = "";
+        document.getElementById("value").value = "";
+        updated = false;
     }
 }
 
@@ -39,66 +74,89 @@ function addNumber(number) {
     input += number;
 }
 
-/* Eventlisteners */
+/* Eventlisteners for the buttons */
 
 const num0 = document.getElementById("zero");
 num0.addEventListener("click", function(){
+    checkIfUpdated();
+    document.getElementById("value").value += "0";
     input += "0";
 });
 
 const num1 = document.getElementById("one");
 num1.addEventListener("click", function(){
+    checkIfUpdated();
+    document.getElementById("value").value += "1";
     input += "1";
 });
 
 const num2 = document.getElementById("two");
 num2.addEventListener("click", function(){
+    checkIfUpdated();
+    document.getElementById("value").value += "2";
     input += "2";
 });
 
 const num3 = document.getElementById("three");
 num3.addEventListener("click", function(){
+    checkIfUpdated();
+    document.getElementById("value").value += "3";
     input += "3";
 });
 
 const num4 = document.getElementById("four");
 num4.addEventListener("click", function(){
+    checkIfUpdated();
+    document.getElementById("value").value += "4";
     input += "4";
 });
 
 const num5 = document.getElementById("five");
 num5.addEventListener("click", function(){
+    checkIfUpdated();
+    document.getElementById("value").value += "5";
     input += "5";
 });
 
 const num6 = document.getElementById("six");
 num6.addEventListener("click", function(){
+    checkIfUpdated();
+    document.getElementById("value").value += "6";
     input += "6";
 });
 
 const num7 = document.getElementById("seven");
 num7.addEventListener("click", function(){
+    checkIfUpdated();
+    document.getElementById("value").value += "7";
     input += "7";
 });
 
 const num8 = document.getElementById("eight");
 num8.addEventListener("click", function(){
+    checkIfUpdated();
+    document.getElementById("value").value += "8";
     input += "8";
 });
 
 const num9 = document.getElementById("nine");
 num9.addEventListener("click", function(){
+    checkIfUpdated();
+    document.getElementById("value").value += "9";
     input += "9";
 });
 
 const comma = document.getElementById("comma");
 comma.addEventListener("click", function(){
+    checkIfUpdated();
+    document.getElementById("value").value += ".";
     input += ".";
 });
 
 const addButton = document.getElementById("add");
 addButton.addEventListener("click", function(){
     if (input != "") {
+        document.getElementById("value").value = "";
         inputArray.push(input);
         input = "";
         inputArray.push("add");
@@ -108,6 +166,7 @@ addButton.addEventListener("click", function(){
 const subtractButton = document.getElementById("subtract");
 subtractButton.addEventListener("click", function(){
     if (input != "") {
+        document.getElementById("value").value = "";
         inputArray.push(input);
         input = "";
         inputArray.push("subtract");
@@ -117,6 +176,7 @@ subtractButton.addEventListener("click", function(){
 const multiplyButton = document.getElementById("multiply");
 multiplyButton.addEventListener("click", function(){
     if (input != "") {
+        document.getElementById("value").value = "";
         inputArray.push(input);
         input = "";
         inputArray.push("multiply");
@@ -126,6 +186,7 @@ multiplyButton.addEventListener("click", function(){
 const divideButton = document.getElementById("divide");
 divideButton.addEventListener("click", function(){
     if (input != "") {
+        document.getElementById("value").value = "";
         inputArray.push(input);
         input = "";
         inputArray.push("divide");
@@ -137,5 +198,15 @@ equalsButton.addEventListener("click", function(){
     if (input != "") {
         inputArray.push(input);
         input = "";
+        answer = operate(inputArray);
+        updateDisplay(answer);
     }
 });
+
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", function(){
+    input = "";
+    inputArray = [];
+    answer = "";
+    document.getElementById("value").value = "";
+})
