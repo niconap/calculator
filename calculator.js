aae05c3d31f2c9a19a82e5c2befde773bb361c74
Nico <nico.nap@outlook.com>
2025-01-18 16:40:03 +0100

    feat: implement calculation of the equation

--- calculator.js
+++ calculator.js
@@ -4,6 +4,12 @@ class Calculator {
         this.equation = [];
     }
 
+    get_equation() {
+        // Get the current equation as a string, this is mostly for testing
+        // purposes.
+        return this.equation.join(' ');
+    }
+
     enter(input) {
         // Add an input, such as a number or operator, to the equation.
         const operators = ['+', '-', '*', '/'];
@@ -12,15 +18,63 @@ class Calculator {
         }
 
         if (!isNaN(this.equation[this.equation.length - 1]) && !isNaN(input)) {
-            this.equation[this.equation.length - 1] += input;
+            let num = String(this.equation[this.equation.length - 1])
+            this.equation[this.equation.length - 1] = Number(num + input);
             return;
         }
 
-        this.equation.push(input);
+        if (this.equation.length > 0 || !isNaN(input)) {
+            if (!isNaN(input)) {
+                input = Number(input);
+            }
+            this.equation.push(input);
+        }
     }
 
-    get_equation() {
-        return this.equation;
+    calculate() {
+        // Calculate the result of the equation, making sure that * and / get
+        // precedence over + and -.
+        if (isNaN(this.equation[this.equation.length - 1])) {
+            return 'Error';
+        }
+
+        while (this.equation.indexOf('*') >= 0
+            || this.equation.indexOf('/') >= 0) {
+            let mult_index = this.equation.indexOf('*');
+            let div_index = this.equation.indexOf('/');
+            if (div_index == -1 || mult_index < div_index) {
+                let sub_result = this.equation[mult_index - 1] *
+                    this.equation[mult_index + 1];
+                this.equation[mult_index - 1] = sub_result;
+                this.equation.splice(mult_index, 2);
+            } else {
+                let sub_result = this.equation[div_index - 1] /
+                    this.equation[div_index + 1];
+                this.equation[div_index - 1] = sub_result;
+                this.equation.splice(div_index, 2);
+            }
+        }
+
+        while (this.equation.indexOf('+') >= 0
+            || this.equation.indexOf('-') >= 0) {
+            let plus_index = this.equation.indexOf('+');
+            let min_index = this.equation.indexOf('-');
+            if (min_index == -1 || plus_index < min_index) {
+                let sub_result = this.equation[plus_index - 1] +
+                    this.equation[plus_index + 1];
+                this.equation[plus_index - 1] = sub_result;
+                this.equation.splice(plus_index, 2);
+            } else {
+                let sub_result = this.equation[min_index - 1] -
+                    this.equation[min_index + 1];
+                this.equation[min_index - 1] = sub_result;
+                this.equation.splice(min_index, 2);
+            }
+        }
+
+        let result = this.equation[0];
+        this.equation = [];
+        return result;
     }
 }
 
--- calculator.test.js
+++ calculator.test.js
@@ -2,13 +2,35 @@ const Calculator = require('./calculator.js')
 
 test('Correctly records operations', () => {
     const calc_instance = new Calculator();
+    calc_instance.enter('+');
+    calc_instance.enter('-');
+    calc_instance.enter('*');
+    calc_instance.enter('/');
+    expect(calc_instance.get_equation()).toStrictEqual('')
+    calc_instance.enter(1);
+    calc_instance.enter('+');
+    calc_instance.enter('2');
+    expect(calc_instance.get_equation()).toStrictEqual('1 + 2');
+    calc_instance.enter('a');
+    expect(calc_instance.get_equation()).toStrictEqual('1 + 2');
+    calc_instance.enter(2);
+    expect(calc_instance.get_equation()).toStrictEqual('1 + 22');
+})
+
+test('Correctly calculates result', () => {
+    const calc_instance = new Calculator();
     calc_instance.enter('1');
     calc_instance.enter('+');
     calc_instance.enter('2');
-    console.log(calc_instance.get_equation());
-    expect(calc_instance.get_equation()).toStrictEqual(['1', '+', '2']);
-    calc_instance.enter('a');
-    expect(calc_instance.get_equation()).toStrictEqual(['1', '+', '2']);
+    expect(calc_instance.calculate()).toBe(3)
+    expect(calc_instance.get_equation()).toStrictEqual('')
+    calc_instance.enter('5');
+    calc_instance.enter('+');
     calc_instance.enter('2');
-    expect(calc_instance.get_equation()).toStrictEqual(['1', '+', '22']);
+    calc_instance.enter('*');
+    calc_instance.enter('2');
+    expect(calc_instance.calculate()).toBe(9)
+    calc_instance.enter('2');
+    calc_instance.enter('*');
+    expect(calc_instance.calculate()).toStrictEqual('Error')
 })
